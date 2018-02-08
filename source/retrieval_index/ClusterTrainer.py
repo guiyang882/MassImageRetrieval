8c204e91cff66f5ac46b8df077362c1272798f97
liuguiyangnwpu <liuguiyangnwpu@163.com>
2018-02-08 23:52:24 +0800

    更新了ClusterModel的结果可视化

--- source/retrieval_index/ClusterTrainer.py
+++ source/retrieval_index/ClusterTrainer.py
@@ -13,13 +13,20 @@ importlib.reload(sys)
 
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
 from tensorflow.examples.tutorials.mnist import input_data
 
 from source.retrieval_index.ClusterModel import ClusterModel
 from source.retrieval_index.DataSampler import DataGenerator
 
 
+mnist = input_data.read_data_sets(
+            "/Volumes/projects/DataSets/basic_dataset/", reshape=False)
+mean_data = np.mean(mnist.train.images, axis=0)
+
+
 class ClusterTrainer:
+
     def __init__(self, sample_creator=None, train_model=None):
         self.sample_creator = sample_creator
         self.train_model = train_model
@@ -31,10 +38,10 @@ class ClusterTrainer:
         self.lr = 0.001
 
         self.global_step = tf.Variable(0, trainable=False, name='global_step')
-
+        self.model_param = None
         self.sess = tf.InteractiveSession()
         self.saver = None
-        self.log_save_dir = "./log/"
+        self.log_save_dir = "./log/cluster"
 
     def reload_model(self):
         if os.path.exists(self.log_save_dir + "checkpoint"):
@@ -43,15 +50,12 @@ class ClusterTrainer:
                 self.saver.restore(self.sess, ckpt.model_checkpoint_path)
 
     def start_train(self):
-        mnist = input_data.read_data_sets("/Volumes/projects/DataSets/basic_dataset/", reshape=False)
-        mean_data = np.mean(mnist.train.images, axis=0)
-
-        model_param = self.train_model.build_model()
+        self.model_param = self.train_model.build_model()
 
         optimizer = tf.train.AdamOptimizer(self.lr)
 
-        with tf.control_dependencies([model_param["centers_update_op"]]):
-            train_op = optimizer.minimize(model_param["total_loss"], global_step=self.global_step)
+        with tf.control_dependencies([self.model_param["centers_update_op"]]):
+            train_op = optimizer.minimize(self.model_param["total_loss"], global_step=self.global_step)
 
         summary_op = tf.summary.merge_all()
 
@@ -60,27 +64,32 @@ class ClusterTrainer:
         writer = tf.summary.FileWriter(self.log_save_dir, self.sess.graph)
 
         step = self.sess.run(self.global_step)
-        while step <= self.epochs * len(mnist.train.images) // self.batch_size:
+        N_step_pre_Epoch = len(mnist.train.images) // self.batch_size
+        while step <= self.epochs * N_step_pre_Epoch:
             batch_images, batch_labels = mnist.train.next_batch(self.batch_size)
             _, summary_str, train_acc = self.sess.run(
-                [train_op, summary_op, model_param["acc"]],
+                [train_op, summary_op, self.model_param["acc"]],
                 feed_dict={
-                    model_param["input_images"]: batch_images - mean_data,
-                    model_param["labels"]: batch_labels,
+                    self.model_param["input_images"]: batch_images - mean_data,
+                    self.model_param["labels"]: batch_labels,
                 })
             step += 1
 
             writer.add_summary(summary_str, global_step=step)
 
-            if step % 200 == 0:
+            if (step+1) % 200 == 0:
                 vali_image = mnist.validation.images - mean_data
                 vali_acc = self.sess.run(
-                    model_param["acc"],
+                    self.model_param["acc"],
                     feed_dict={
-                        model_param["input_images"]: vali_image,
-                        model_param["labels"]: mnist.validation.labels
+                        self.model_param["input_images"]: vali_image,
+                        self.model_param["labels"]: mnist.validation.labels
                     })
                 print(("step: {}, train_acc:{:.4f}, vali_acc:{:.4f}".format(step, train_acc, vali_acc)))
+
+            if (step+1) % N_step_pre_Epoch == 0:
+                self.save_model_log(step // N_step_pre_Epoch)
+                self.visualize_results(step)
 
     def save_model_log(self, epoch_id=None):
         if not os.path.isdir(self.log_save_dir):
@@ -90,6 +99,24 @@ class ClusterTrainer:
                             global_step=epoch_id)
         else:
             self.saver.save(self.sess, self.log_save_dir + "model.ckpt")
+
+    def visualize_results(self, step):
+        feat = self.sess.run(
+            self.model_param["cluster"],
+            feed_dict={
+                self.model_param["input_images"]: mnist.train.images[:10000] - mean_data
+            })
+        labels = mnist.train.labels[:10000]
+
+        f = plt.figure(figsize=(16, 9))
+        c = ['#ff0000', '#ffff00', '#00ff00', '#00ffff', '#0000ff',
+             '#ff00ff', '#990000', '#999900', '#009900', '#009999']
+        for i in range(10):
+            plt.plot(feat[labels == i, 0].flatten(),
+                     feat[labels == i, 1].flatten(), '.', c=c[i])
+        plt.legend(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])
+        plt.grid()
+        plt.savefig("../../experiment/cluster/" + str(step) + "_vis.png")
 
 
 if __name__ == '__main__':
